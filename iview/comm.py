90455259ad33a0e5e304537f7b130760ab791bcf
Ben McGinnes <ben@adversary.org>
2014-11-06 09:55:03 +1100

    Fixed tab/space issue in these 4 files as well.
    Conflicts:
    	iview/comm.py
    	iview/config.py

--- iview/comm.py
+++ iview/comm.py
@@ -12,168 +12,168 @@ from .utils import http_get
 iview_config = None
 
 def fetch_url(url, types=None):
-	"""	Simple function that fetches a URL using urllib.
-		An exception is raised if an error (e.g. 404) occurs.
-	"""
-	url = urljoin(config.base_url, url)
-	headers = iview_config['headers']
-	
-	# Not using plain urlopen() because the combination of
-	# urlopen()'s "Connection: close" header and
-	# a "gzip" encoded response
-	# sometimes seems to cause the server to truncate the HTTP response
-	from .utils import PersistentConnectionHandler
-	with PersistentConnectionHandler() as connection:
-		session = urllib.request.build_opener(connection)
-		with http_get(session, url, types, headers=headers) as http:
-			headers = http.info()
-			if headers.get('content-encoding') == 'gzip':
-				return gzip.GzipFile(fileobj=http).read()
-			else:
-				return http.read()
+    """    Simple function that fetches a URL using urllib.
+        An exception is raised if an error (e.g. 404) occurs.
+    """
+    url = urljoin(config.base_url, url)
+    headers = iview_config['headers']
+    
+    # Not using plain urlopen() because the combination of
+    # urlopen()'s "Connection: close" header and
+    # a "gzip" encoded response
+    # sometimes seems to cause the server to truncate the HTTP response
+    from .utils import PersistentConnectionHandler
+    with PersistentConnectionHandler() as connection:
+        session = urllib.request.build_opener(connection)
+        with http_get(session, url, types, headers=headers) as http:
+            headers = http.info()
+            if headers.get('content-encoding') == 'gzip':
+                return gzip.GzipFile(fileobj=http).read()
+            else:
+                return http.read()
 
 def maybe_fetch(url, type=None):
-	"""	Only fetches a URL if it is not in the cache directory.
-		In practice, this is really bad, and only useful for saving
-		bandwidth when debugging. For one, it doesn't respect
-		HTTP's wishes. Also, iView, by its very nature, changes daily.
-	"""
+    """    Only fetches a URL if it is not in the cache directory.
+        In practice, this is really bad, and only useful for saving
+        bandwidth when debugging. For one, it doesn't respect
+        HTTP's wishes. Also, iView, by its very nature, changes daily.
+    """
 
-	if not config.cache:
-		return fetch_url(url, type)
+    if not config.cache:
+        return fetch_url(url, type)
 
-	if not os.path.isdir(config.cache):
-		os.mkdir(config.cache)
+    if not os.path.isdir(config.cache):
+        os.mkdir(config.cache)
 
-	filename = os.path.join(config.cache, url.rsplit('/', 1)[-1])
+    filename = os.path.join(config.cache, url.rsplit('/', 1)[-1])
 
-	if os.path.isfile(filename):
-		with open(filename, 'rb') as f:
-			data = f.read()
-	else:
-		data = fetch_url(url, type)
-		with open(filename, 'wb') as f:
-			f.write(data)
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            data = f.read()
+    else:
+        data = fetch_url(url, type)
+        with open(filename, 'wb') as f:
+            f.write(data)
 
-	return data
+    return data
 
 def get_config(headers=()):
-	"""	This function fetches the iView "config". Among other things,
-		it tells us an always-metered "fallback" RTMP server, and points
-		us to many of iView's other XML files.
-	"""
-	global iview_config
+    """    This function fetches the iView "config". Among other things,
+        it tells us an always-metered "fallback" RTMP server, and points
+        us to many of iView's other XML files.
+    """
+    global iview_config
 
-	headers = dict(headers)
-	try:
-		headers['User-Agent'] = headers['User-Agent'] + ' '
-	except LookupError:
-		headers['User-Agent'] = ''
-	headers['User-Agent'] += config.user_agent
-	headers['Accept-Encoding'] = 'gzip'
-	iview_config = dict(headers=headers)
-	
-	xml = maybe_fetch(config.config_url, ("application/xml", "text/xml"))
-	parsed = parser.parse_config(xml)
-	iview_config.update(parsed)
+    headers = dict(headers)
+    try:
+        headers['User-Agent'] = headers['User-Agent'] + ' '
+    except LookupError:
+        headers['User-Agent'] = ''
+    headers['User-Agent'] += config.user_agent
+    headers['Accept-Encoding'] = 'gzip'
+    iview_config = dict(headers=headers)
+    
+    xml = maybe_fetch(config.config_url, ("application/xml", "text/xml"))
+    parsed = parser.parse_config(xml)
+    iview_config.update(parsed)
 
 def get_auth():
-	""" This function performs an authentication handshake with iView.
-		Among other things, it tells us if the connection is unmetered,
-		and gives us a one-time token we need to use to speak RTSP with
-		ABC's servers, and tells us what the RTMP URL is.
-	"""
-	auth = iview_config['auth_url']
-	if config.ip:
-		query = urlsplit(auth).query
-		query = query and query + "&"
-		query += urlencode((("ip", config.ip),))
-		auth = urljoin(auth, "?" + query)
-	auth = fetch_url(auth, ("application/xml", "text/xml"))
-	return parser.parse_auth(auth, iview_config)
+    """ This function performs an authentication handshake with iView.
+        Among other things, it tells us if the connection is unmetered,
+        and gives us a one-time token we need to use to speak RTSP with
+        ABC's servers, and tells us what the RTMP URL is.
+    """
+    auth = iview_config['auth_url']
+    if config.ip:
+        query = urlsplit(auth).query
+        query = query and query + "&"
+        query += urlencode((("ip", config.ip),))
+        auth = urljoin(auth, "?" + query)
+    auth = fetch_url(auth, ("application/xml", "text/xml"))
+    return parser.parse_auth(auth, iview_config)
 
 def get_categories():
-	"""Returns the list of categories
-	"""
-	url = iview_config['categories_url']
-	category_data = maybe_fetch(url, ("application/xml", "text/xml"))
-	categories = parser.parse_categories(category_data)
-	return categories
+    """Returns the list of categories
+    """
+    url = iview_config['categories_url']
+    category_data = maybe_fetch(url, ("application/xml", "text/xml"))
+    categories = parser.parse_categories(category_data)
+    return categories
 
 def get_index():
-	"""	This function pulls in the index, which contains the TV series
-		that are available to us. Returns a list of "dict" objects,
-		one for each series.
-	"""
-	return series_api('seriesIndex')
+    """    This function pulls in the index, which contains the TV series
+        that are available to us. Returns a list of "dict" objects,
+        one for each series.
+    """
+    return series_api('seriesIndex')
 
 def get_series_items(series_id, get_meta=False):
-	"""	This function fetches the series detail page for the selected series,
-		which contain the items (i.e. the actual episodes). By
-		default, returns a list of "dict" objects, one for each
-		episode. If "get_meta" is set, returns a tuple with the first
-		element being the list of episodes, and the second element a
-		"dict" object of series infomation.
-	"""
+    """    This function fetches the series detail page for the selected series,
+        which contain the items (i.e. the actual episodes). By
+        default, returns a list of "dict" objects, one for each
+        episode. If "get_meta" is set, returns a tuple with the first
+        element being the list of episodes, and the second element a
+        "dict" object of series infomation.
+    """
 
-	series = series_api('series', series_id)
+    series = series_api('series', series_id)
 
-	for meta in series:
-		if meta['id'] == series_id:
-			break
-	else:
-		# Bad series number returns empty json string, ignore it.
-		print('no results for series id {}, skipping'.format(series_id), file=sys.stderr)
-		return []
-	
-	items = meta['items']
-	if get_meta:
-		return (items, meta)
-	else:
-		return items
+    for meta in series:
+        if meta['id'] == series_id:
+            break
+    else:
+        # Bad series number returns empty json string, ignore it.
+        print('no results for series id {}, skipping'.format(series_id), file=sys.stderr)
+        return []
+    
+    items = meta['items']
+    if get_meta:
+        return (items, meta)
+    else:
+        return items
 
 def get_keyword(keyword):
-	return series_api('keyword', keyword)
+    return series_api('keyword', keyword)
 
 def series_api(key, value=""):
-	query = urlencode(((key, value),))
-	url = urljoin(iview_config['api_url'], '?' + query)
-	index_data = maybe_fetch(url, ("application/json",))
-	return parser.parse_series_api(index_data)
+    query = urlencode(((key, value),))
+    url = urljoin(iview_config['api_url'], '?' + query)
+    index_data = maybe_fetch(url, ("application/json",))
+    return parser.parse_series_api(index_data)
 
 def get_highlights():
-	# Reported as Content-Type: text/html
-	highlightXML = maybe_fetch(iview_config['highlights'])
-	return parser.parse_highlights(highlightXML)
+    # Reported as Content-Type: text/html
+    highlightXML = maybe_fetch(iview_config['highlights'])
+    return parser.parse_highlights(highlightXML)
 
 def get_captions(url):
-	"""	This function takes a program name (e.g. news/730report_100803) and
-		fetches the corresponding captions file. It then passes it to
-		parse_subtitle(), which converts it to SRT format.
-	"""
+    """    This function takes a program name (e.g. news/730report_100803) and
+        fetches the corresponding captions file. It then passes it to
+        parse_subtitle(), which converts it to SRT format.
+    """
 
-	captions_url = '{}{}.xml'.format(iview_config['captions_url'], url)
+    captions_url = '{}{}.xml'.format(iview_config['captions_url'], url)
 
-	TYPES = ("text/xml", "application/xml")
-	xml = maybe_fetch(captions_url, TYPES)
-	return parser.parse_captions(xml)
+    TYPES = ("text/xml", "application/xml")
+    xml = maybe_fetch(captions_url, TYPES)
+    return parser.parse_captions(xml)
 
 def configure_socks_proxy():
-	"""	Import the modules necessary to support usage of a SOCKS proxy
-		and configure it using the current settings in iview.config
-		NOTE: It would be safe to call this function multiple times
-		from, say, a GTK settings dialog
-	"""
-	try:
-		import socks
-		import socket
-		socket.socket = socks.socksocket
-	except:
-		sys.excepthook(*sys.exc_info())
-		print("The Python SOCKS client module is required for proxy support.", file=sys.stderr)
-		sys.exit(3)
+    """    Import the modules necessary to support usage of a SOCKS proxy
+        and configure it using the current settings in iview.config
+        NOTE: It would be safe to call this function multiple times
+        from, say, a GTK settings dialog
+    """
+    try:
+        import socks
+        import socket
+        socket.socket = socks.socksocket
+    except:
+        sys.excepthook(*sys.exc_info())
+        print("The Python SOCKS client module is required for proxy support.", file=sys.stderr)
+        sys.exit(3)
 
-	socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, config.socks_proxy_host, config.socks_proxy_port)
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, config.socks_proxy_host, config.socks_proxy_port)
 
 if config.socks_proxy_host is not None:
-	configure_socks_proxy()
+    configure_socks_proxy()
